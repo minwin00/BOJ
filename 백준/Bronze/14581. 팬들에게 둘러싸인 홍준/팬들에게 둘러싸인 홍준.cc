d15329ed17e876d944ad48e9249acbbf154cd18f
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-03-15 00:42:21 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	string s;
+	cin >> s;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::"<<s<<"::fan:\n";
+	cout << ":fan::fan::fan:";
+	return (0);
+}
