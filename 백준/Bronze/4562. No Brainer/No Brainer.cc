26dab51ab2fc59d8bdff61bd0076f33e459afe1a
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-04-16 15:10:16 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	int a, b;
+	cin >> n;
+	for (int i = 0; i<n; i++)
+	{
+		cin >> a >> b;
+		if(a<b) cout << "NO BRAINS\n";
+		else cout << "MMM BRAINS\n";
+	}
+	return (0);
+}
