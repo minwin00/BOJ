e21c7c54dcc3130ea459f5a90c72340bcbec8dc3
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-05-01 16:27:22 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	string s;
+	cin >> s;
+	if (s== "SONGDO") cout << "HIGHSCHOOL";
+	else if (s== "CODE") cout << "MASTER";
+	else if (s== "2023") cout << "0611";
+	else if (s== "ALGORITHM") cout << "CONTEST";
+	return (0);
+}
