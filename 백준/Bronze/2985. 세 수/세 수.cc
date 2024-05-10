89bbd9db9912ec12b0d8f58e3a153943ffacf81b
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-05-10 14:09:09 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if ( a+b== c)  cout << a << "+" << b << "=" << c;
+	else if ( a-b== c)  cout << a << "-" << b << "=" << c;
+	else if ( a*b== c)  cout << a << "*" << b << "=" << c;
+	else if ( a/b== c)  cout << a << "/" << b << "=" << c;
+	else if ( b+c == a)  cout << a << "=" << b << "+" << c;
+	else if ( b-c == a)  cout << a << "=" << b << "-" << c;
+	else if ( b*c == a)  cout << a << "=" << b << "*" << c;
+	else if ( b/c == a)  cout << a << "=" << b << "/" << c;
+	return (0);
+}
