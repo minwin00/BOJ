5b712a618496b7848f67d3030f2465330799120a
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-07-03 18:47:58 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	char c;
+	cin >> c;
+	if (c == 'M') cout << "MatKor";
+	if (c == 'W') cout << "WiCys";
+	if (c == 'C') cout << "CyKor";
+	if (c == 'A') cout << "AlKor";
+	if (c == '$') cout << "$clear";
+	return 0;
+}
