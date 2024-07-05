065b56de91625599927a973560293b37849110dd
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-07-05 12:48:37 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	int n;
+	int a;
+	cin >>n;
+	for (int i = 0; i<n; i++){
+		cin >> a;
+		cout << a << " " << a<<"\n";
+	}
+	return 0;
+}
