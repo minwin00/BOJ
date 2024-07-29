efe969f35f950576e32c9822113e04cc95c8cf2a
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-07-29 23:58:15 +0900

    [Gold IV] Title: 탈출, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.cc
+++ 백준/Gold/3055. 탈출/탈출.cc
@@ -1,52 +1,43 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 #include <algorithm>
-#include <vector>
+
 using namespace std;
 
-int n, m;
-char a[51][51];
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+int r, c;
+char board[51][51];
+int vst[51][51];
+int wvst[51][51];
+queue<pair<pair<int, int>, char> > q;
 
-
-void bfs(int wx, int wy, int water[][51]){
-	queue<pair<int, int> > q;
-	q.push(make_pair(wx, wy));
-	water[wx][wy] = 1;
-	while (!q.empty()){
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-		for (int i =0; i<4; i++){
-			int nx = x+dx[i];
-			int ny = y+dy[i];
-			if (nx >=0 && ny>=0 && nx<n&&ny<m){
-				if(a[nx][ny] == '.' && (water[nx][ny] >water[x][y] +1)){
-					water[nx][ny] =water[x][y] +1;
-					q.push(make_pair(nx, ny));
-				}
-			}
-		}
-	}
-	
-}
-
-void bfs2(int sx, int sy, int vst[][51], int water[][51]){
-	queue<pair<int, int> > q;
-	q.push(make_pair(sx, sy));
+void bfs(int sx, int sy){
+	q.push(make_pair(make_pair(sx, sy), 'S'));
 	vst[sx][sy] = 1;
-	while (!q.empty()){
-		int x = q.front().first;
-		int y = q.front().second;
+	while(!q.empty()){
+		int x = q.front().first.first;
+		int y = q.front().first.second;
+		char sym = q.front().second;
 		q.pop();
-		for (int i =0; i<4; i++){
+		for (int i=0; i<4; i++){
 			int nx = x+dx[i];
 			int ny = y+dy[i];
-			if (nx >=0 && ny>=0 && nx<n&&ny<m){
-				if((a[nx][ny] == '.'|| a[nx][ny] == 'D') && (vst[nx][ny] >vst[x][y] +1) && (water[nx][ny] > vst[x][y] +1)){
-					vst[nx][ny] =vst[x][y] +1;
-					q.push(make_pair(nx, ny));
+			if(nx>=0&&ny>=0&&nx<r&&ny<c){
+				if (board[nx][ny] == '.'){
+					if(!wvst[nx][ny] && sym == '*') {
+						board[nx][ny] = '*';
+						wvst[nx][ny] =1;
+						q.push(make_pair(make_pair(nx, ny), sym));
+					}
+					if(!vst[nx][ny] && sym == 'S'){
+						vst[nx][ny] = vst[x][y]+1;
+						q.push(make_pair(make_pair(nx, ny), sym));
+					}
+				}
+				else if (board[nx][ny] == 'D' && sym=='S' && !vst[nx][ny]){
+					vst[nx][ny] = vst[x][y];
 				}
 			}
 		}
@@ -54,43 +45,28 @@ void bfs2(int sx, int sy, int vst[][51], int water[][51]){
 }
 
 int main(void){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int water[51][51] ={0,};
-	int vst[51][51] ={0,};
-	vector<pair<int, int> > v;
-	char c;
-	int sx, sy;
-	int ex, ey;
-	int wx, wy;
-	cin >> n >> m;
-	for (int i =0; i<n; i++){
-		for (int j =0; j<m; j++){
-			cin >> c;
-			a[i][j] =c;
-			vst[i][j] = 9999999;
-			water[i][j] = 9999999;
-			if (a[i][j] == 'D'){
-				ex = i;
-				ey = j;
-			}
-			if (a[i][j] == 'S'){
+	char s;
+	int sx, sy, ex, ey;
+	cin >> r>> c;
+	for (int i=0; i<r; i++){
+		for (int j=0; j<c;j++){
+			cin >> s;
+			board[i][j] = s;
+			if (s == 'S'){
 				sx = i;
 				sy = j;
 			}
-			if (a[i][j] == '*'){
-				wx = i;
-				wy = j;
-				v.push_back(make_pair(wx, wy));
+			if (s == '*'){
+				q.push(make_pair(make_pair(i, j),'*'));
+			}
+			if (s== 'D'){
+				ex = i;
+				ey = j;
 			}
 		}
 	}
-	for (int i =0; i<v.size(); i++){
-		bfs(v[i].first, v[i].second, water);
-	}
-	bfs2(sx, sy, vst, water);
-	if (vst[ex][ey] == 9999999) cout << "KAKTUS";
-	else cout << vst[ex][ey]-1;
+	bfs(sx, sy);
+	if (vst[ex][ey]==0) cout << "KAKTUS";
+	else cout << vst[ex][ey];
 	return 0;
 }
