34a501a1557421214e684c65c8a75f430bb09791
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-08-11 23:59:22 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	string s;
+	cin >> s;
+	if (s=="NLCS") cout << "North London Collegiate School";
+	else if (s=="BHA") cout << "Branksome Hall Asia";
+	else if (s=="KIS") cout << "Korea International School";
+	else if (s=="SJA") cout << "St. Johnsbury Academy";
+	return 0;
+}
