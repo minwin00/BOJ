061717d3bb0e849c0ce0fd4cd322984cd8043974
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-08-13 23:47:24 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	int n;
+	int a;
+	int sum = 0;
+	cin >> n;
+	for (int i=0; i<n; i++){
+		cin >> a;
+		sum += a;
+	}
+	cout << sum;
+	return 0;
+}
