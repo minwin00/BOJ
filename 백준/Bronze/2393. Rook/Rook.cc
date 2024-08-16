1491e55cb6eb03a9d412be038baf3024ea876c95
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-08-17 00:40:47 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std; 
+
+int main(void){
+	cout <<"  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\";
+	return 0;
+}
