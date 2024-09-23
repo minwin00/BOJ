f6689c88ab256c093ab1f460504608b06567a7fd
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-09-23 14:55:39 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	int n;
+	cin >> n;
+	for (int i=0; i<n; i++){
+		for (int i=0; i<n; i++){
+			cout << "@@@@@";
+		}
+		cout << "\n";
+	}
+
+	for (int i=0;i<n; i++){
+		for (int i=0; i<n; i++){
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int i=0; i<n; i++){
+		for (int i=0; i<n; i++){
+			cout << "@@@@@";
+		}
+		cout << "\n";
+	}
+	for (int i=0;i<n; i++){
+		for (int i=0; i<n; i++){
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int i=0;i<n; i++){
+		for (int i=0; i<n; i++){
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
