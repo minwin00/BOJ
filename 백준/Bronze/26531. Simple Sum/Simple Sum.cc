b76b790c23e94d3c9ce18d94adc262a7209585fa
minwin00 <89986641+minwin00@users.noreply.github.com>
2024-10-07 16:41:10 +0900

    [Bronze IV] Title: Simple Sum, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26531. Simple Sum/Simple Sum.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	int a, b, c;
+	char x, y;
+	cin >> a >> x >> b >> y >> c;
+	if (a+b==c) cout << "YES";
+	else cout << "NO";
+	return 0;
+}
